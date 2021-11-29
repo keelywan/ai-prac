98b76c943ee2da9331305cb0cf400fc24fa025fa
Keely <keelywan00@gmail.com>
2021-11-29 17:22:56 -0500

    Add searching

--- data/authorization_code/app.js
+++ data/authorization_code/app.js
@@ -13,16 +13,16 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'TODO'; // Your client id
-var client_secret = 'TODO'; // Your secret
-var redirect_uri = 'TODO'; // Your redirect uri
+var client_id = '2494ba0082084c0389788b20839b8735'; // Your client id
+var client_secret = 'f5dfc9d81a9541d2add152a6373742da'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
-var generateRandomString = function(length) {
+var generateRandomString = function (length) {
     var text = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
@@ -40,7 +40,7 @@ app.use(express.static(__dirname + '/public'))
     .use(cors())
     .use(cookieParser());
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
 
     var state = generateRandomString(16);
     res.cookie(stateKey, state);
@@ -57,7 +57,7 @@ app.get('/login', function(req, res) {
         }));
 });
 
-app.get('/callback', function(req, res) {
+app.get('/callback', function (req, res) {
 
     // your application requests refresh and access tokens
     // after checking the state parameter
@@ -86,7 +86,7 @@ app.get('/callback', function(req, res) {
             json: true
         };
 
-        request.post(authOptions, function(error, response, body) {
+        request.post(authOptions, function (error, response, body) {
             if (!error && response.statusCode === 200) {
 
                 var access_token = body.access_token,
@@ -99,7 +99,7 @@ app.get('/callback', function(req, res) {
                 };
 
                 // use the access token to access the Spotify Web API
-                request.get(options, function(error, response, body) {
+                request.get(options, function (error, response, body) {
                     console.log(body);
                 });
 
@@ -119,7 +119,7 @@ app.get('/callback', function(req, res) {
     }
 });
 
-app.get('/refresh_token', function(req, res) {
+app.get('/refresh_token', function (req, res) {
 
     // requesting access token from refresh token
     var refresh_token = req.query.refresh_token;
@@ -133,7 +133,7 @@ app.get('/refresh_token', function(req, res) {
         json: true
     };
 
-    request.post(authOptions, function(error, response, body) {
+    request.post(authOptions, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             var access_token = body.access_token;
             res.send({
